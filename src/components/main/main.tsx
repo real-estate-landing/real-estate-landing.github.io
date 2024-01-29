159d78cb5fbe30201dba051c3195c740fe67f74a
Mirsaid <mir21.07.2004@gmail.com>
2024-01-29 16:18:50 +0500

    debug: link preload as attribute

--- src/components/main/main.tsx
+++ src/components/main/main.tsx
@@ -5,7 +5,6 @@ import Partners from "./components/partners/partners";
 import Comments from "./components/comments/comments";
 import Subscription from "./components/subscription/subscription";
 import Faq from "./components/faq/faq";
-import { useEffect } from "react";
 
 export default function Main() {
   return (
